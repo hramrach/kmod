7360112fc1420f9bddd7ca5b916c1e80e3d2b40c
Michal Marek <mmarek@suse.cz>
2014-02-26 13:48:55 +0100

    modprobe: Recognize --allow-unsupported-modules on commandline
    The option does not do anything yet, but it does not return error
    either.
    
    References: fate#316971
    Patch-mainline: never

--- tools/modprobe.c
+++ tools/modprobe.c
@@ -84,6 +84,8 @@ static const struct option cmdopts[] = {
 	{"dirname", required_argument, 0, 'd'},
 	{"set-version", required_argument, 0, 'S'},
 
+	{"allow-unsupported-modules", no_argument, 0, 128},
+
 	{"syslog", no_argument, 0, 's'},
 	{"quiet", no_argument, 0, 'q'},
 	{"verbose", no_argument, 0, 'v'},
@@ -808,6 +810,9 @@ static int do_modprobe(int argc, char **orig_argv)
 			break;
 		case 'S':
 			kversion = optarg;
+			break;
+		case 128:
+			/* --allow-unsupported-modules does nothing for now */
 			break;
 		case 's':
 			env_modprobe_options_append("-s");
