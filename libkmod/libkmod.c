bcd7b96c34f60c4789a7461d1d1c4af1a8db4bda
Michal Suchanek <msuchanek@suse.de>
2023-05-26 10:38:05 +0200

    libkmod, depmod, modprobe: Make directory for kernel modules configurable
    Now that modprobe.d is searched under ${prefix}/lib, allow a complete
    transition to files only under ${prefix} by adding a ${module_directory}
    configuration. This specifies the directory where to search for kernel
    modules and should match the location where the kernel/distro installs
    them.
    
    With this distributions that do not want to ship files in /lib can also
    move kernel modules to /usr while others can keep them in /lib.
    
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>
    ---
    v4: Make the whole path configurable
    v5: More verbose commit message

--- libkmod/libkmod.c
+++ libkmod/libkmod.c
@@ -209,7 +209,7 @@ static int log_priority(const char *priority)
 	return 0;
 }
 
-static const char *dirname_default_prefix = "/lib/modules";
+static const char *dirname_default_prefix = MODULE_DIRECTORY;
 
 static char *get_kernel_release(const char *dirname)
 {
@@ -231,7 +231,7 @@ static char *get_kernel_release(const char *dirname)
 /**
  * kmod_new:
  * @dirname: what to consider as linux module's directory, if NULL
- *           defaults to /lib/modules/`uname -r`. If it's relative,
+ *           defaults to ${module_prefix}/lib/modules/`uname -r`. If it's relative,
  *           it's treated as relative to the current working directory.
  *           Otherwise, give an absolute dirname.
  * @config_paths: ordered array of paths (directories or files) where
